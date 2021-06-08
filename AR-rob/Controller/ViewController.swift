dd88616656a88b1dfa1656bc06f9e1377e98f5aa
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-06-08 10:52:22 +0700

    Fix contraint sceneview, add backbutton and hidden stat

--- AR-rob/Controller/ViewController.swift
+++ AR-rob/Controller/ViewController.swift
@@ -12,6 +12,7 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var backButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,13 +21,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.showsStatistics = false
         
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        backButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        backButton.tintColor = .white
     }
     
     override func viewWillAppear(_ animated: Bool) {
