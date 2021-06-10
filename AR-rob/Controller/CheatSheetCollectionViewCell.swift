1bc001fa36332d7d28717607d2845e78ee3dd7a7
Arya <aryailham05@gmail.com>
2021-06-10 12:12:01 +0700

    ADD: CheatSheetCell xib and controller

---
+++ AR-rob/Controller/CheatSheetCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CheatSheetCollectionViewCell.swift
+//  AR-rob
+//
+//  Created by Arya Ilham on 10/06/21.
+//
+
+import UIKit
+
+class CheatSheetCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cheatTitle: UILabel!
+    @IBOutlet weak var cheatImage: UIImageView!
+    @IBOutlet weak var cheatDetail: UITextView!
+}
