03d057b41a00587c493078cbfd90b6d9958b020f
Arya <aryailham05@gmail.com>
2021-06-13 21:24:25 +0700

    ADD: Other UI material according to design

--- AR-rob/Controller/CheatSheetCollectionViewCell.swift
+++ AR-rob/Controller/CheatSheetCollectionViewCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class CheatSheetCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var cheatTitle: UILabel!
+    @IBOutlet weak var cheatSubTitle1: UILabel!
+    @IBOutlet weak var cheatSubTitle2: UILabel!
     @IBOutlet weak var cheatImage: UIImageView!
     @IBOutlet weak var cheatDetail: UITextView!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    
 }
