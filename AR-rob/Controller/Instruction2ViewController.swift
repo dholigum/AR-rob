d11e3a05ac0ef46c634d62b74ec9fd4e2d38c231
sholost <stoedirman@gmail.com>
2021-06-13 23:31:41 +0700

    transition done

--- AR-rob/Controller/Instruction2ViewController.swift
+++ AR-rob/Controller/Instruction2ViewController.swift
@@ -20,16 +20,36 @@ class Instruction2ViewController: UIViewController {
         let onboardStoryboard: UIStoryboard = UIStoryboard(name: "Instruction", bundle: nil)
         let onboardView = onboardStoryboard.instantiateViewController(identifier: "instructionView") as! InstructionViewController
         
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromLeft
+        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.linear)
+        view.window!.layer.add(transition, forKey: kCATransition)
+        
+//        let transition = CATransition()
+//        transition.type = CATransitionType.push
+//        transition.subtype = CATransitionSubtype.fromLeft
+//        transition.duration = 1
+//        view.layer.add(transition, forKey: "transition")
+//
         onboardView.modalPresentationStyle = .fullScreen
-        self.present(onboardView, animated: true, completion: nil)
+        self.present(onboardView, animated: false, completion: nil)
     }
     
     @IBAction func pressedNext(_ sender: Any) {
         let onboardStoryboard: UIStoryboard = UIStoryboard(name: "Instruction", bundle: nil)
         let onboardView = onboardStoryboard.instantiateViewController(identifier: "Instruction3") as! Instruction3ViewController
         
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromRight
+        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+        view.window!.layer.add(transition, forKey: kCATransition)
+        
         onboardView.modalPresentationStyle = .fullScreen
-        self.present(onboardView, animated: true, completion: nil)
+        self.present(onboardView, animated: false, completion: nil)
     }
     
 
