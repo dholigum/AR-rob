4b50e4072845db6806d8b0b7db77cc736bd028fa
Arya <aryailham05@gmail.com>
2021-06-14 10:50:35 +0700

    ADD: Textview padding settings to 0

--- AR-rob/Controller/CheatSheetViewController.swift
+++ AR-rob/Controller/CheatSheetViewController.swift
@@ -21,25 +21,35 @@ class CheatSheetViewController: UIViewController, UICollectionViewDelegate {
         
         let nib = UINib(nibName: "\(CheatSheetCollectionViewCell.self)", bundle: nil)
         cheatSheetCollectionView.register(nib, forCellWithReuseIdentifier: "CheatSheetCell")
+        
+        let summaryNib = UINib(nibName: "\(SummaryCheatSheetCollectionViewCell.self)", bundle: nil)
+        cheatSheetCollectionView.register(summaryNib, forCellWithReuseIdentifier: "SummaryCell")
     }
 }
 
 extension CheatSheetViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cheatSheetMaterial.count
+        return cheatSheetMaterial.count + 1
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CheatSheetCell", for: indexPath) as! CheatSheetCollectionViewCell
-            
-        cell.cheatTitle.text = "\(cheatSheetMaterial[indexPath.row].materialName)"
-        cell.cheatSubTitle1.text = "\(cheatSheetMaterial[indexPath.row].materialSubTitle1)"
-        cell.cheatSubTitle2.text = "\(cheatSheetMaterial[indexPath.row].materialSubTitle2)"
-        cell.cheatDetail.text = "\(cheatSheetMaterial[indexPath.row].materialDetail)"
-        cell.summaryLabel.text = "\(cheatSheetMaterial[indexPath.row].materialQuickSummary)"
+        if indexPath.row < cheatSheetMaterial.count{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CheatSheetCell", for: indexPath) as! CheatSheetCollectionViewCell
+                
+            cell.cheatTitle.text = "\(cheatSheetMaterial[indexPath.row].materialName)"
+            cell.cheatSubTitle1.text = "\(cheatSheetMaterial[indexPath.row].materialSubTitle1)"
+            cell.cheatSubTitle2.text = "\(cheatSheetMaterial[indexPath.row].materialSubTitle2)"
+            cell.cheatDetail.text = "\(cheatSheetMaterial[indexPath.row].materialDetail)"
+            cell.cheatDetail.textContainer.lineFragmentPadding = 0
+            cell.summaryLabel.text = "\(cheatSheetMaterial[indexPath.row].materialQuickSummary)"
         
-        return cell
+            return cell
+        }
+        else{
+            return collectionView.dequeueReusableCell(withReuseIdentifier: "SummaryCell", for: indexPath) as! SummaryCheatSheetCollectionViewCell
+        }
+        
     }
 
 }
